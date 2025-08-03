7f4afdbed11502ff0e998e0bcbd4da814515de67
kirilllob <lobuzov230510@gmail.com>
2025-08-03 15:29:16 +0300

    исправил кавычки

--- render_website.py
+++ render_website.py
@@ -13,11 +13,11 @@ def on_reload():
     with open("meta_data.json", "r", encoding="utf8") as my_file:
         books = json.load(my_file)
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"])
     )
     os.makedirs("pages", exist_ok=True)
-    template = env.get_template('template.html')
+    template = env.get_template("template.html")
     quantity_page = 10
     books_pages = list(chunked(books, quantity_page))
     for number, books_page in enumerate(books_pages):
@@ -26,16 +26,16 @@ def on_reload():
             all_numbers = len(books_pages),
             number_page = number+1
         )
-        with open(f'pages/index{number+1}.html', 'w', encoding="utf8") as file:
+        with open(f"pages/index{number+1}.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
 
 
 def main():
     on_reload()
     server = Server()
-    server.watch('template.html', on_reload)
-    server.serve(root='.', default_filename="./pages/index1.html")
+    server.watch("template.html", on_reload)
+    server.serve(root=".", default_filename="./pages/index1.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
